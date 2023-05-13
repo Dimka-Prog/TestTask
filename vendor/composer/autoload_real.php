e7da2ffdbeb2e584d5ad606fbc4aacffcb781cbe
Dimka-Prog <dima.sozonov.95@mail.ru>
2023-05-13 23:07:16 +1000

    Добавление зависимости symfony/console в composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit7253a27a73041a5592c0e9c0cac4d3a9
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit7253a27a73041a5592c0e9c0cac4d3a9', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7253a27a73041a5592c0e9c0cac4d3a9', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit7253a27a73041a5592c0e9c0cac4d3a9
         call_user_func(\Composer\Autoload\ComposerStaticInit7253a27a73041a5592c0e9c0cac4d3a9::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit7253a27a73041a5592c0e9c0cac4d3a9::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
